be067169657a3c80c5c29b224338bb0e17ca4b4f
Jakhongir <67756831+Jakhongir-1984@users.noreply.github.com>
2022-03-22 15:39:39 +0500

    add

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,10 @@ $(function () {
   $('.header__btn-menu').on('click', function () {
     $('.menu').toggleClass('menu--open');
   });
+
+  if($(window).width() < 651) {
+    $('.works-path__item--measuring').appendTo($('.works-path__items-box'));
+  }
   
   $('.top__slider').slick({
     dots: true,
